233f58b69ba0de94842073e26e0019e53b6c710a
Mauricio-Maciel-Soares <mauriciomacielsoares@yahoo.com.br>
2022-05-02 16:49:10 -0300

    req 6 pronto

--- index.js
+++ index.js
@@ -45,27 +45,22 @@ app.post('/talker', isValidToken, isValidName, isValidAge, thereIsKeyTalk, isVal
   newTalker.id = talkers.length + 1; // chave do id dinâmica;
   talkers.push(newTalker);
   await writing(talkers);
-  console.log(talkers);
   return response.status(201).json(newTalker);
 });
+
+// https:app.betrybe.com/course/back-end/introducao-ao-desenvolvimento-web-com-nodejs/express-http-com-nodejs/8022a9b1-7548-4298-97ce-9acfa8986e66/conteudos/6538b037-f4ae-4cb8-a237-1a1501d996f4/atualizando-e-deletando-objetos-atraves-da-api/1c0f51c5-d076-4635-b3d3-b57979d164ba?use_case=side_bar
 
 app.put('/talker/:id', isValidToken, isValidName, isValidAge, thereIsKeyTalk, isValidKeys,
   isValidDateRate, async (request, response) => {
   const { id } = request.params;
-  const { name, age, talk: { watchedAt, rate } } = request.body;
-  const editedTalker = {
-    id,
-    name,
-    age,
-    talk: {
-      watchedAt,
-      rate,
-    },
-  };
+  const { name, age, talk } = request.body;
+  const editedTalker = { id: Number(id), name, age, talk };
   const talkers = await reading();
-  talkers.push(editedTalker);
+  const talkerId = talkers.findIndex((e) => e.id === parseInt(id, 10));
+  if (talkerId === -1) return response.status(404).json({ message: 'talker not found!' });
+
+  talkers[talkerId] = { ...talkers[talkerId], name, age, talk };
   await writing(talkers);
-  console.log(talkers);
   return response.status(200).json(editedTalker);
 });
 
