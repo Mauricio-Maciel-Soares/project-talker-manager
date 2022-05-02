cbc27dde22f2f407c68746ff9f954ae6bb59a44b
Mauricio-Maciel-Soares <mauriciomacielsoares@yahoo.com.br>
2022-05-02 19:05:41 -0300

    req 8 pronto

--- utils/router.js
+++ utils/router.js
@@ -16,6 +16,13 @@ const {
 const HTTP_OK_STATUS = 200;
 const router = express.Router();
 
+router.get('/search', isValidToken, async (request, response) => {
+  const { q } = request.query;
+  const talkers = await reading();
+  const filteredTalkers = talkers.filter((e) => e.name.includes(q));
+  response.status(200).json(filteredTalkers);
+});
+
 router.get('/', async (_request, response) => {
   const talkers = await reading();
   response.status(HTTP_OK_STATUS).json(talkers);
@@ -55,19 +62,6 @@ router.post('/talker', isValidToken, isValidName, isValidAge, thereIsKeyTalk, is
   return response.status(201).json(newTalker);
 });
 
-// https:app.betrybe.com/course/back-end/introducao-ao-desenvolvimento-web-com-nodejs/express-http-com-nodejs/8022a9b1-7548-4298-97ce-9acfa8986e66/conteudos/6538b037-f4ae-4cb8-a237-1a1501d996f4/atualizando-e-deletando-objetos-atraves-da-api/1c0f51c5-d076-4635-b3d3-b57979d164ba?use_case=side_bar
-
-router.delete('/talker/:id', isValidToken, async (request, response) => {
-  const { id } = request.params;
-  const talkers = await reading();
-  const talkerId = talkers.findIndex((e) => e.id === parseInt(id, 10));
-  if (talkerId === -1) return response.status(404).json({ message: 'talker not found!' });
-
-  talkers.splice(talkerId, id);
-  await writing(talkers);
-  return response.status(204).end();
-});
-
 router.put('/talker/:id', isValidToken, isValidName, isValidAge, thereIsKeyTalk, isValidKeys,
   isValidDateRate, async (request, response) => {
   const { id } = request.params;
@@ -80,6 +74,19 @@ router.put('/talker/:id', isValidToken, isValidName, isValidAge, thereIsKeyTalk,
   talkers[talkerId] = { ...talkers[talkerId], name, age, talk };
   await writing(talkers);
   return response.status(200).json(editedTalker);
+});
+
+// https:app.betrybe.com/course/back-end/introducao-ao-desenvolvimento-web-com-nodejs/express-http-com-nodejs/8022a9b1-7548-4298-97ce-9acfa8986e66/conteudos/6538b037-f4ae-4cb8-a237-1a1501d996f4/atualizando-e-deletando-objetos-atraves-da-api/1c0f51c5-d076-4635-b3d3-b57979d164ba?use_case=side_bar
+
+router.delete('/talker/:id', isValidToken, async (request, response) => {
+  const { id } = request.params;
+  const talkers = await reading();
+  const talkerId = talkers.findIndex((e) => e.id === parseInt(id, 10));
+  if (talkerId === -1) return response.status(404).json({ message: 'talker not found!' });
+
+  talkers.splice(talkerId, id);
+  await writing(talkers);
+  return response.status(204).end();
 });
 
 module.exports = router;
